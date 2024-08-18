21be1861e92be27082632b1bcd68cb719a9e9762
ataher83 <taher83@gmail.com>
2024-08-18 15:50:24 +0600

    .env.local file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,14 +4,23 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCx2lT8gBpfebtGCykw44rVfb7HRsB0-7A",
+//   authDomain: "natural-agro.firebaseapp.com",
+//   projectId: "natural-agro",
+//   storageBucket: "natural-agro.appspot.com",
+//   messagingSenderId: "205202897644",
+//   appId: "1:205202897644:web:d73dd09c811c88caa003c9"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCx2lT8gBpfebtGCykw44rVfb7HRsB0-7A",
-  authDomain: "natural-agro.firebaseapp.com",
-  projectId: "natural-agro",
-  storageBucket: "natural-agro.appspot.com",
-  messagingSenderId: "205202897644",
-  appId: "1:205202897644:web:d73dd09c811c88caa003c9"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
