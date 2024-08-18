a88d74c05ab87c5ea538a48b2f037eaba927de57
ataher83 <taher83@gmail.com>
2024-08-18 15:41:26 +0600

    firebase.config.js file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCx2lT8gBpfebtGCykw44rVfb7HRsB0-7A",
+  authDomain: "natural-agro.firebaseapp.com",
+  projectId: "natural-agro",
+  storageBucket: "natural-agro.appspot.com",
+  messagingSenderId: "205202897644",
+  appId: "1:205202897644:web:d73dd09c811c88caa003c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
