b41ede32132e5e25c5a64989f6d074e862eb9a21
ataher83 <taher83@gmail.com>
2024-08-18 21:00:47 +0600

    firebase configuration & hosting

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -11,32 +11,9 @@ const firebaseConfig = {
     storageBucket: import.meta.env.VITE_STORAGEBUCKET,
     messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
     appId: import.meta.env.VITE_APPID,
-
-//   apiKey: "AIzaSyAIJvcJkR5VUGTrXQJqPv0ozF01FmnEoUI",
-//   authDomain: "the-art-gallery-74571.firebaseapp.com",
-//   projectId: "the-art-gallery-74571",
-//   storageBucket: "the-art-gallery-74571.appspot.com",
-//   messagingSenderId: "257907237444",
-//   appId: "1:257907237444:web:0138f1d153a2b6bd195211"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
-
-
-
-
-
-
-
-// new config to setup
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDJt5xs7wDifmht6pTZL1ANJiRPUQlJEtE",
-//   authDomain: "the-alt-products.firebaseapp.com",
-//   projectId: "the-alt-products",
-//   storageBucket: "the-alt-products.appspot.com",
-//   messagingSenderId: "670767849706",
-//   appId: "1:670767849706:web:9c7f92c2b1154b74727793"
-// };
 
